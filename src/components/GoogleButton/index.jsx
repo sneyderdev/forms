68a776bc2d2aea8fd259507661833c51940aca39
Sneyder Barreto <sneyderbarreto@gmail.com>
2021-04-08 16:40:21 -0500

    FIX: Set the text of the GoogleButton based on the route

--- src/components/GoogleButton/index.jsx
+++ src/components/GoogleButton/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import googleIcon from '@images/google-icon.png';
 
 import Button from './GoogleButton.styles';
 
-const GoogleButton = () => (
-  <div>
-    <form>
-      <Button type="button">
-        <img src={googleIcon} alt="Google" />
-        Sign in with Google
-      </Button>
-    </form>
-  </div>
-);
+const GoogleButton = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div>
+      <form>
+        <Button type="button">
+          <img src={googleIcon} alt="Google" />
+          {pathname === '/login'
+            ? 'Sign in with Google'
+            : 'Sign up with Google'}
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 export default GoogleButton;
